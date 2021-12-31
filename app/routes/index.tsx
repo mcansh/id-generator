dee1404b6682aec6b9673c386e233cd2b3e067fd
Logan McAnsh <logan@mcan.sh>
2021-12-31 18:55:19 -0500

    chore: add support for getting more than a single cuid
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,53 +1,82 @@
 import cuid from "cuid";
-import { Form, json, LoaderFunction, useLoaderData } from "remix";
+import { Form, json, LoaderFunction, useLoaderData, useLocation } from "remix";
 import { copyToClipboard } from "copy-lite";
 
 interface RouteData {
-  cuid: string;
+  cuids: Array<string>;
 }
 
-let loader: LoaderFunction = () => {
-  return json<RouteData>({ cuid: cuid() });
+let loader: LoaderFunction = ({ request }) => {
+  let url = new URL(request.url);
+  let count = parseInt(url.searchParams.get("count") || "1");
+
+  let cuids = [...Array.from({ length: count })].map(() => cuid());
+
+  return json<RouteData>({ cuids: cuids });
 };
 
 function IndexPage() {
   let data = useLoaderData<RouteData>();
+  let single = data?.cuids.length === 1;
+  let location = useLocation();
 
   return (
-    <main className="container flex flex-col min-h-screen p-4 mx-auto">
+    <main className="flex flex-col max-w-screen-sm min-h-screen p-4 mx-auto">
       <h1 className="text-4xl">CUID Generator</h1>
 
-      <div className="flex-auto mt-2 md:flex-none">
-        <label htmlFor="cuid" className="block text-xl">
-          Here is your CUID:
-        </label>
-        <div className="flex mt-2">
-          <input
-            type="text"
-            id="cuid"
-            className="w-full p-2 border border-zinc-400 rounded-l-md"
+      {single ? (
+        <>
+          <div className="flex-auto mt-2 md:flex-none">
+            <label htmlFor="cuid" className="block text-xl">
+              Here is your CUID:
+            </label>
+            <div className="flex mt-2">
+              <input
+                type="text"
+                id="cuid"
+                className="w-full p-2 border-t border-b border-l border-zinc-400 rounded-l-md"
+                readOnly
+                value={data.cuids[0]}
+              />
+              <button
+                type="button"
+                className="px-4 py-2 text-white bg-indigo-500 border-t border-b border-r border-zinc-400 rounded-r-md"
+                onClick={() => {
+                  return copyToClipboard(data.cuids[0]);
+                }}
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <textarea
+            className="w-full p-2 border rounded-md resize-none border-zinc-400"
             readOnly
-            value={data.cuid}
+            value={data.cuids.join("\n")}
+            rows={data.cuids.length}
           />
           <button
             type="button"
-            className="px-4 py-2 text-white bg-indigo-500 rounded-r-md"
+            className="block px-4 py-2 mt-2 text-white bg-indigo-500 rounded-md"
             onClick={() => {
-              return copyToClipboard(data.cuid);
+              return copyToClipboard(data.cuids.join("\n"));
             }}
           >
             Copy
           </button>
-        </div>
-      </div>
+        </>
+      )}
 
       <div className="mt-2">
-        <Form>
+        <Form replace action={`${location.pathname}${location.search}`}>
           <button
             className="w-full px-4 py-2 text-white bg-indigo-500 rounded-md md:w-auto"
             type="submit"
           >
-            Get another
+            Get {single ? "another" : `${data.cuids.length} more`}
           </button>
         </Form>
       </div>
