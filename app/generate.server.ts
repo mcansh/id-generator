1e9aa63f7b243393d786290350f14f5dc35300b8
Logan McAnsh <logan@mcan.sh>
2023-01-12 11:44:33 -0500

    feat: add hyperid support
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/generate.server.ts
+++ app/generate.server.ts
@@ -1,17 +1,20 @@
 import cuid from "cuid";
 import { v4 as uuid } from "@lukeed/uuid";
 import { nanoid } from "nanoid";
-import { createId } from "@paralleldrive/cuid2";
+import { createId as cuid2 } from "@paralleldrive/cuid2";
+import createHyperIdInstance from "hyperid";
 
-let idTypes = ["cuid", "cuid2", "uuid", "nanoid"] as const;
+let hyperid = createHyperIdInstance();
 
-type IdType = typeof idTypes[number];
+export let idTypes = ["cuid", "cuid2", "uuid", "nanoid", "hyperid"] as const;
+
+export type IdType = typeof idTypes[number];
 
 function makeArray(length: number) {
   return [...Array.from({ length })];
 }
 
-function generateIds(type: IdType, count: number) {
+export function generateIds(type: IdType, count: number) {
   switch (type) {
     case "cuid": {
       return makeArray(count).map(() => cuid());
@@ -23,13 +26,13 @@ function generateIds(type: IdType, count: number) {
       return makeArray(count).map(() => nanoid());
     }
     case "cuid2": {
-      return makeArray(count).map(() => createId());
+      return makeArray(count).map(() => cuid2());
+    }
+    case "hyperid": {
+      return makeArray(count).map(() => hyperid());
     }
     default: {
       throw new Error(`Unknown type: ${type}`);
     }
   }
 }
-
-export { generateIds, idTypes };
-export type { IdType };
